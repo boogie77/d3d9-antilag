8b130def80f8efe3b856c29cde2f4eab8da896ff
iceboy <me@iceboy.org>
2017-09-09 21:34:33 +0800

    add obs compatibility in an intrusive way

--- src/create.c
+++ src/create.c
@@ -13,21 +13,44 @@ static HRESULT (WINAPI *orig_d3d9_CreateDevice)(
     D3DPRESENT_PARAMETERS *pPresentationParameters,
     IDirect3DDevice9 **ppReturnedDeviceInterface);
 
-typedef struct {
-    void (*after_present)(IDirect3DDevice9 *device);
-} extension_ptrs_t;
+static HRESULT (WINAPI *orig_d3d9_device_Present)(
+    IDirect3DDevice9 *This,
+    const RECT *pSourceRect,
+    const RECT *pDestRect,
+    HWND hDestWindowOverride,
+    const RGNDATA *pDirtyRegion);
+
+static HRESULT (*obs_get_backbuffer)(
+    IDirect3DDevice9 *device,
+    IDirect3DSurface9 **surface);
+
+static void (*obs_d3d9_capture)(
+    IDirect3DDevice9 *device,
+    IDirect3DSurface9 *surface);
 
 static LARGE_INTEGER perf_freq;
 static LARGE_INTEGER start_time;
 static IDirect3DDevice9 *device;
 static IDirect3DDevice9Vtbl *device_vtbl;
-extension_ptrs_t *volatile extension_ptrs;
 
 #define UPDATE_ELAPSED_TICKS() \
     do { \
         QueryPerformanceCounter(&end_time); \
         elapsed_ticks = (end_time.QuadPart - start_time.QuadPart) * 10000000 / perf_freq.QuadPart; \
     } while (0)
+
+void handle_obs(uintptr_t reloc_offset)
+{
+    MP_PROTECT_BEGIN(orig_d3d9_device_Present, 5);
+    *(uint32_t *)orig_d3d9_device_Present = 0x8b55ff8b;
+    *(uint8_t *)((uint32_t)orig_d3d9_device_Present + 4) = 0xec;
+    MP_PROTECT_END(orig_d3d9_device_Present, 5);
+
+    obs_get_backbuffer = (void *)(0x5c667440 + reloc_offset);
+    obs_d3d9_capture = (void *)(0x5c666680 + reloc_offset);
+    volatile uint8_t *obs_stop_loop = (void *)(0x5c6a08ad + reloc_offset);
+    *obs_stop_loop = 1;
+}
 
 HRESULT WINAPI d3d9_swap_chain_Present(
     IDirect3DSwapChain9 *This,
@@ -37,7 +60,7 @@ HRESULT WINAPI d3d9_swap_chain_Present(
     const RGNDATA *pDirtyRegion,
     DWORD dwFlags)
 {
-    HRESULT hr = device_vtbl->Present(
+    HRESULT hr = orig_d3d9_device_Present(
         device,
         pSourceRect,
         pDestRect,
@@ -47,10 +70,27 @@ HRESULT WINAPI d3d9_swap_chain_Present(
     int64_t elapsed_ticks;
     UPDATE_ELAPSED_TICKS();
     int64_t expected_ticks = 133333 + (elapsed_ticks - TARGET_LATENCY_TICKS) / 5;
-    extension_ptrs_t *ext = extension_ptrs;
-    if (ext) {
-        ext->after_present(device);
+
+    // OBS compatibility.
+    if (obs_get_backbuffer) {
+        IDirect3DSurface9 *backbuffer;
+        obs_get_backbuffer(device, &backbuffer);
+        obs_d3d9_capture(device, backbuffer);
+        backbuffer->lpVtbl->Release(backbuffer);
+    } else if (*(uint16_t *)orig_d3d9_device_Present == 0xf9eb) {
+        uintptr_t jmp_slot = (uintptr_t)orig_d3d9_device_Present - 4;
+        uintptr_t jmp_base = (uintptr_t)orig_d3d9_device_Present;
+        uintptr_t jmp_address = jmp_base + *(uintptr_t *)jmp_slot;
+        uintptr_t reloc_offset = jmp_address - 0x5c667550;
+        handle_obs(reloc_offset);
+    } else if (*(uint8_t *)orig_d3d9_device_Present == 0xe9) {
+        uintptr_t jmp_slot = (uintptr_t)orig_d3d9_device_Present + 1;
+        uintptr_t jmp_base = (uintptr_t)orig_d3d9_device_Present + 5;
+        uintptr_t jmp_address = jmp_base + *(uintptr_t *)jmp_slot;
+        uintptr_t reloc_offset = jmp_address - 0x5c667550;
+        handle_obs(reloc_offset);
     }
+
     UPDATE_ELAPSED_TICKS();
     if (elapsed_ticks <= expected_ticks) {
         if (expected_ticks - elapsed_ticks >= 30000) {
@@ -88,6 +128,7 @@ HRESULT WINAPI d3d9_CreateDevice(
     }
 
     device_vtbl = device->lpVtbl;
+    orig_d3d9_device_Present = device_vtbl->Present;
     device_vtbl->GetSwapChain(device, 0, &swap_chain);
     swap_chain_vtbl = swap_chain->lpVtbl;
     MP_PROTECT_BEGIN(swap_chain_vtbl, sizeof(*swap_chain_vtbl));
@@ -104,6 +145,7 @@ IDirect3D9 *WINAPI Direct3DCreate9(UINT SDKVersion)
     IDirect3D9Vtbl *d3d9_vtbl;
 
     SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
+    SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_ABOVE_NORMAL);
     QueryPerformanceFrequency(&perf_freq);
 
     d3d9 = orig_Direct3DCreate9(SDKVersion);
